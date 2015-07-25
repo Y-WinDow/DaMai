e3081a6a75b951c775be3de4e8da476d17d25136
window <sky_window@163.com>
2015-07-25 09:51:50 +0800

    add app2

---
+++ app2.js
@@ -0,0 +1,76 @@
+var events = require('./modules/widget/events');
+var tplParser = require('./modules/plugins/tplParser');
+
+function App(opts){
+	this.init(opts);
+}
+App.prototype.init = function (opts){
+	this.pages = [];
+	this.inEffects = [];
+	this.cache = [];
+	
+	this.APICaches = {};
+	
+	this.currentPage = opts.currentPage || 0;
+};
+
+App.prototype.toPage = function (index){
+	if(index === this.currentPage){
+		return;
+	}
+	if(!this.cache[index]){
+		this.pages[index] = new Page({
+			parent: this,
+			url: this.urls[index],
+			inEffect: this.inEffects[index]
+		});
+		this.toPage(index);
+		return;
+	}
+	this.pages[this.currentPage].out();
+	this.pages[index].in();
+	this.currentPage = index;
+};
+
+App.prototype.cache = function (){
+	// application cahce // HTML5 API not local storage
+};
+App.prototype.API = function (url){
+	if(this.APICaches[url]){
+		
+	}
+	
+};
+
+function Page(opts){
+	this.init(opts);
+}
+
+Page.prototype.init = function (opts){
+	this.app = opts.app
+	this.url = opts.url;
+	this.param = opts.param;
+	this.pageNum = opts.pageNum;
+	this.data = {};
+	this.inEffect = opts.inEffect || 'blank';
+	this.outEffect = opts.outEffect || 'blank';
+	
+	this.$ele = document.createElement('div');
+};
+Page.prototype.load = function (){
+	this.emit('loading...');
+	var self = this;
+	if(!this.xhr){
+		this.xhr = new XMLHttpRequest();
+		this.xhr.onload = function (){
+			self.data = this.responseText;
+			self.emit('load');
+		};
+	}
+	this.xhr.open(this.method, this.url, false);
+};
+Page.prototype.bindEvents = function (){
+	this.on('load', function (){
+		this.parent.cache[this.index] = this.pageData;
+	});
+};
